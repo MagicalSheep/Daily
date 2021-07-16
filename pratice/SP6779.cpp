71a23e864e9e32514c3d96a71263f386cdde3ac0
MagicalSheep <magical_sheep@outlook.com>
2021-07-16 20:29:11 +0800

    SP6779

--- pratice/SP6779.cpp
+++ pratice/SP6779.cpp
@@ -19,101 +19,6 @@ void addEdge(ll u, ll v)
     head[u] = cnt;
 }
 
-struct node
-{
-    ll l, r, lm, rm, sum, ans, s, flag;
-} tree[MAXN << 2];
-
-void pushup(ll p)
-{
-    tree[p].sum = tree[p << 1].sum + tree[p << 1 | 1].sum;
-    tree[p].lm = max(tree[p << 1].lm, tree[p << 1].sum + tree[p << 1 | 1].lm);
-    tree[p].rm = max(tree[p << 1 | 1].rm, tree[p << 1 | 1].sum + tree[p << 1].rm);
-    tree[p].ans = max(max(tree[p << 1].ans, tree[p << 1 | 1].ans), tree[p << 1].rm + tree[p << 1 | 1].lm);
-}
-
-void pushdown(ll p)
-{
-    if (!tree[p].flag)
-        return;
-    tree[p << 1].sum = tree[p].s * (tree[p << 1].r - tree[p << 1].l + 1);
-    tree[p << 1 | 1].sum = tree[p].s * (tree[p << 1 | 1].r - tree[p << 1 | 1].l + 1);
-    tree[p << 1].lm = tree[p << 1].rm = tree[p << 1].ans = max((ll)0, tree[p << 1].sum);
-    tree[p << 1 | 1].lm = tree[p << 1 | 1].rm = tree[p << 1 | 1].ans = max((ll)0, tree[p << 1 | 1].sum);
-    tree[p << 1].s = tree[p << 1 | 1].s = tree[p].s;
-    tree[p].s = 0;
-}
-
-void build(ll p, ll s, ll t)
-{
-    tree[p].l = s;
-    tree[p].r = t;
-    tree[p].s = 0;
-    tree[p].flag = 0;
-    if (s == t)
-    {
-        tree[p].lm = tree[p].rm = tree[p].ans = tree[p].sum = aa[rnk[s]];
-        return;
-    }
-    ll mid = (s + t) >> 1;
-    build(p << 1, s, mid), build(p << 1 | 1, mid + 1, t);
-    pushup(p);
-}
-
-void update(ll p, ll s, ll t, ll c)
-{
-    if (tree[p].l >= s && tree[p].r <= t)
-    {
-        tree[p].s = c;
-        tree[p].flag = 1;
-        tree[p].sum = c * (tree[p].r - tree[p].l + 1);
-        tree[p].lm = tree[p].rm = tree[p].ans = max((ll)0, tree[p].sum);
-        return;
-    }
-    pushdown(p);
-    ll mid = (tree[p].r + tree[p].l) >> 1;
-    if (s <= mid)
-        update(p << 1, s, t, c);
-    if (t > mid)
-        update(p << 1 | 1, s, t, c);
-    pushup(p);
-}
-
-node merge(node x, node y)
-{
-    node ret;
-    ret.sum = x.sum + y.sum;
-    ret.lm = max(x.lm, x.sum + y.lm);
-    ret.rm = max(y.rm, y.sum + x.rm);
-    ret.ans = max(max(x.ans, y.ans), x.rm + y.lm);
-    return ret;
-}
-
-node query(int p, int s, int t)
-{
-    if (tree[p].l >= s && tree[p].r <= t)
-        return tree[p];
-    pushdown(p);
-    ll mid = (tree[p].l + tree[p].r) >> 1;
-    node ret;
-    if (s > mid)
-    {
-        ret = query(p << 1 | 1, s, t);
-    }
-    else if (t <= mid)
-    {
-        ret = query(p << 1, s, t);
-    }
-    else
-    {
-        node n1, n2;
-        n1 = query(p << 1, s, t);
-        n2 = query(p << 1 | 1, s, t);
-        ret = merge(n1, n2);
-    }
-    return ret;
-}
-
 void dfs1(ll p)
 {
     son[p] = -1;
@@ -146,6 +51,88 @@ void dfs2(ll p, ll t)
             dfs2(Edge[e].to, Edge[e].to);
 }
 
+struct node
+{
+    ll l, r, lm, rm, sum, ans, s, flag;
+    node() { lm = rm = sum = ans = 0; }
+} tree[MAXN << 2];
+
+node merge(node x, node y)
+{
+    node ret;
+    ret.l = x.l;
+    ret.r = y.r;
+    ret.sum = x.sum + y.sum;
+    ret.lm = max(x.lm, x.sum + y.lm);
+    ret.rm = max(y.rm, y.sum + x.rm);
+    ret.ans = max(max(x.ans, y.ans), x.rm + y.lm);
+    ret.s = ret.flag = 0;
+    return ret;
+}
+
+void build(ll p, ll s, ll t)
+{
+    tree[p].l = s;
+    tree[p].r = t;
+    tree[p].s = 0;
+    if (s == t)
+    {
+        tree[p].sum = aa[rnk[s]];
+        tree[p].lm = tree[p].rm = tree[p].ans = max((ll)0, aa[rnk[s]]);
+        tree[p].flag = 0;
+        return;
+    }
+    ll mid = (s + t) >> 1;
+    build(p << 1, s, mid), build(p << 1 | 1, mid + 1, t);
+    tree[p] = merge(tree[p << 1], tree[p << 1 | 1]);
+}
+
+void pushdown(ll p)
+{
+    if (!tree[p].flag)
+        return;
+    tree[p << 1].sum = tree[p].s * (tree[p << 1].r - tree[p << 1].l + 1);
+    tree[p << 1 | 1].sum = tree[p].s * (tree[p << 1 | 1].r - tree[p << 1 | 1].l + 1);
+    tree[p << 1].lm = tree[p << 1].rm = tree[p << 1].ans = max((ll)0, tree[p << 1].sum);
+    tree[p << 1 | 1].lm = tree[p << 1 | 1].rm = tree[p << 1 | 1].ans = max((ll)0, tree[p << 1 | 1].sum);
+    tree[p << 1].s = tree[p << 1 | 1].s = tree[p].s;
+    tree[p << 1].flag = tree[p << 1 | 1].flag = 1; // note: fresh flag
+    tree[p].s = tree[p].flag = 0;
+}
+
+void update(ll p, ll s, ll t, ll c)
+{
+    if (tree[p].l >= s && tree[p].r <= t)
+    {
+        tree[p].sum = c * (tree[p].r - tree[p].l + 1);
+        tree[p].lm = tree[p].rm = tree[p].ans = max((ll)0, tree[p].sum);
+        tree[p].s = c;
+        tree[p].flag = 1;
+        return;
+    }
+    pushdown(p);
+    ll mid = (tree[p].r + tree[p].l) >> 1;
+    if (s <= mid)
+        update(p << 1, s, t, c);
+    if (t > mid)
+        update(p << 1 | 1, s, t, c);
+    tree[p] = merge(tree[p << 1], tree[p << 1 | 1]);
+}
+
+node query(int p, int s, int t)
+{
+    if (tree[p].l >= s && tree[p].r <= t)
+        return tree[p];
+    pushdown(p);
+    ll mid = (tree[p].l + tree[p].r) >> 1;
+    node L, R;
+    if (s <= mid)
+        L = query(p << 1, s, t);
+    if (t > mid)
+        R = query(p << 1 | 1, s, t);
+    return merge(L, R);
+}
+
 void modify(ll a, ll b, ll c)
 {
     ll fu = top[a], fv = top[b];
@@ -168,8 +155,6 @@ ll getans(ll a, ll b)
 {
     ll fu = top[a], fv = top[b];
     node L, R;
-    L.ans = L.lm = L.rm = L.sum = 0;
-    R.ans = R.lm = R.rm = R.sum = 0;
     while (fu != fv)
     {
         if (dep[fu] >= dep[fv])
@@ -189,32 +174,32 @@ ll getans(ll a, ll b)
 
 int main()
 {
-    cin >> N;
+    scanf("%lld", &N);
     for (int i = 1; i <= N; i++)
-        cin >> aa[i];
+        scanf("%lld", &aa[i]);
     for (int i = 1; i < N; i++)
     {
         ll u, v;
-        cin >> u >> v;
+        scanf("%lld%lld", &u, &v);
         addEdge(u, v), addEdge(v, u);
     }
     dep[1] = 1;
     dfs1(1), dfs2(1, 1);
     build(1, 1, N);
-    cin >> Q;
+    scanf("%lld", &Q);
     while (Q--)
     {
         ll o;
-        cin >> o;
+        scanf("%lld", &o);
         ll a, b, c;
         if (o == 1)
         {
-            cin >> a >> b;
+            scanf("%lld%lld", &a, &b);
             cout << getans(a, b) << endl;
         }
         else
         {
-            cin >> a >> b >> c;
+            scanf("%lld%lld%lld", &a, &b, &c);
             modify(a, b, c);
         }
     }
