b3a0163a6d721986d73970dab341e7db648120d5
MagicalSheep <magical_sheep@outlook.com>
2021-07-22 09:32:25 +0800

    POJ3683

--- pratice/POJ3683.cpp
+++ pratice/POJ3683.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <stdlib.h>
 #include <string.h>
 using namespace std;
-const int MAXN = 1e4 + 10;
+const int MAXN = 1e6 + 10;
 
 int D[MAXN], N, start[MAXN];
 int cnt, head[MAXN];
