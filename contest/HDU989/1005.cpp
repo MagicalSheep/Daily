0f6fd31fae0581f6d1380a3acbfc3cec91962674
MagicalSheep <magical_sheep@outlook.com>
2021-08-05 20:45:35 +0800

    Contest: HDU989-1005

--- contest/HDU989/1005.cpp
+++ contest/HDU989/1005.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 const int MAXN = 1e5 + 10;
 
-int T, n, m, b[MAXN];
+int T, n, m, b[MAXN], block[MAXN];
 
 int main()
 {
@@ -12,53 +12,24 @@ int main()
         scanf("%d%d", &n, &m);
         for (int i = 1; i <= m; i++)
             scanf("%d", &b[i]);
-        if (n == m)
-        {
-            printf("YES\n");
-            continue;
-        }
-        int l, r, left = 0, right = 0;
-        bool isok = false;
         sort(b + 1, b + 1 + m);
-        b[m + 1] = b[m];
-        for (int i = 1; i <= m; i++)
+        b[m + 1] = n + 1;
+        for (int i = 1; i <= m + 1; i++)
+            block[i] = b[i] - b[i - 1] - 1;
+        int s = 0, mx = 0, mxi = 0;
+        for (int i = 1; i <= m + 1; i++)
         {
-            l = b[i] - i - left;
-            r = n - b[i] - (m - i) - right;
-            int delta = b[i + 1] - b[i] - 1;
-            if (l == r || l + 1 == r)
-            {
-                isok = true;
-                break;
-            }
-            if (l > r)
-            {
-                isok = false;
-                break;
-            }
-            if (l == 0)
-            {
-                if (r == 1)
-                {
-                    isok = true;
-                    break;
-                }
-                left = n - r - m;
-                right = 0;
-                continue;
-            }
-            if (l + 1 <= delta)
-            {
-                left = 2 * l + 1;
-                right = 0;
-            }
-            else
-            {
-                left = l + delta;
-                right = l - delta + 1;
-            }
+            if (mx < block[i])
+                mx = block[i], mxi = i;
+            s += block[i];
         }
-        printf((isok) ? "YES\n" : "NO\n");
+        s -= mx;
+        if (mx <= s)
+            printf("YES\n");
+        else if (mxi - 1 >= mx - s)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
     return 0;
 }
