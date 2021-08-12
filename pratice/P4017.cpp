55f4040b0409de392a8e6bf9e62526c552a6cf51
MagicalSheep <magical_sheep@outlook.com>
2021-08-12 16:32:33 +0800

    P4017

---
+++ pratice/P4017.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+/* --------------- fast io begin--------------- */
+namespace Fread
+{
+    const int SIZE = 1 << 21;
+    char buf[SIZE], *S, *T;
+    inline char getchar()
+    {
+        if (S == T)
+        {
+            T = (S = buf) + fread(buf, 1, SIZE, stdin);
+            if (S == T)
+                return '\n';
+        }
+        return *S++;
+    }
+} // namespace Fread
+namespace Fwrite
+{
+    const int SIZE = 1 << 21;
+    char buf[SIZE], *S = buf, *T = buf + SIZE;
+    inline void flush()
+    {
+        fwrite(buf, 1, S - buf, stdout);
+        S = buf;
+    }
+    inline void putchar(char c)
+    {
+        *S++ = c;
+        if (S == T)
+            flush();
+    }
+    struct NTR
+    {
+        ~NTR() { flush(); }
+    } ztr;
+} // namespace Fwrite
+#ifdef ONLINE_JUDGE
+#define getchar Fread ::getchar
+#define putchar Fwrite ::putchar
+#endif
+namespace Fastio
+{
+    struct Reader
+    {
+        template <typename T>
+        Reader &operator>>(T &x)
+        {
+            char c = getchar();
+            T f = 1;
+            while (c < '0' || c > '9')
+            {
+                if (c == '-')
+                    f = -1;
+                c = getchar();
+            }
+            x = 0;
+            while (c >= '0' && c <= '9')
+            {
+                x = x * 10 + (c - '0');
+                c = getchar();
+            }
+            x *= f;
+            return *this;
+        }
+        Reader &operator>>(char &c)
+        {
+            c = getchar();
+            while (c == '\n' || c == ' ')
+                c = getchar();
+            return *this;
+        }
+        Reader &operator>>(char *str)
+        {
+            int len = 0;
+            char c = getchar();
+            while (c == '\n' || c == ' ')
+                c = getchar();
+            while (c != '\n' && c != ' ')
+            {
+                str[len++] = c;
+                c = getchar();
+            }
+            str[len] = '\0';
+            return *this;
+        }
+        Reader() {}
+    } cin;
+    const char endl = '\n';
+    struct Writer
+    {
+        template <typename T>
+        Writer &operator<<(T x)
+        {
+            if (x == 0)
+            {
+                putchar('0');
+                return *this;
+            }
+            if (x < 0)
+            {
+                putchar('-');
+                x = -x;
+            }
+            static int sta[45];
+            int top = 0;
+            while (x)
+            {
+                sta[++top] = x % 10;
+                x /= 10;
+            }
+            while (top)
+            {
+                putchar(sta[top] + '0');
+                --top;
+            }
+            return *this;
+        }
+        Writer &operator<<(char c)
+        {
+            putchar(c);
+            return *this;
+        }
+        Writer &operator<<(char *str)
+        {
+            int cur = 0;
+            while (str[cur])
+                putchar(str[cur++]);
+            return *this;
+        }
+        Writer &operator<<(const char *str)
+        {
+            int cur = 0;
+            while (str[cur])
+                putchar(str[cur++]);
+            return *this;
+        }
+        Writer() {}
+    } cout;
+} // namespace Fastio
+#define cin Fastio ::cin
+#define cout Fastio ::cout
+#define endl Fastio ::endl
+/* --------------- fast io end--------------- */
+
+#define P 80112002
+const int MAXN = 5e3 + 10;
+const int MAXM = 5e5 + 10;
+
+int n, m, cnt, head[MAXN], ind[MAXN], f[MAXN];
+ll ans;
+
+struct edge
+{
+    int to, next;
+} Edge[MAXM];
+
+void addEdge(int u, int v)
+{
+    Edge[++cnt].to = v;
+    Edge[cnt].next = head[u];
+    head[u] = cnt;
+}
+
+ll dfs(int u)
+{
+    ll tot = 0;
+    if (!head[u])
+        return 1;
+    if (f[u])
+        return f[u];
+    for (int e = head[u]; e; e = Edge[e].next)
+        tot = (tot + dfs(Edge[e].to)) % P;
+    return f[u] = tot;
+}
+
+int main()
+{
+    cin >> n >> m;
+    while (m--)
+    {
+        int A, B;
+        cin >> A >> B;
+        addEdge(B, A);
+        ind[A]++;
+    }
+    for (int i = 1; i <= n; i++)
+        if (!ind[i])
+            ans = (ans + dfs(i)) % P;
+    cout << ans << endl;
+    return 0;
+}
