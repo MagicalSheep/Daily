47ac101a836d94615fac856aef5bdaf8dde5448b
MagicalSheep <wen_cheng_ke@126.com>
2021-11-07 22:20:39 +0800

    ABC226A

---
+++ pratice/ABC226A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+const int MAXN = 1e5 + 10;
+
+double x;
+
+int main()
+{
+    scanf("%lf", &x);
+    double ans = round(x);
+    printf("%.0f\n", ans);
+    return 0;
+}
