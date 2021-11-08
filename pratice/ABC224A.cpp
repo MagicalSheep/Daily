14e59d46db01f65d8af6a2be5b09b4951fdf218c
MagicalSheep <wen_cheng_ke@126.com>
2021-11-08 10:06:39 +0800

    ABC224A

---
+++ pratice/ABC224A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+int main()
+{
+    cin >> s;
+    if (s.back() == 'r')
+        cout << "er" << endl;
+    else
+        cout << "ist" << endl;
+    return 0;
+}
