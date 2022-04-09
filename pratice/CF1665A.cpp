74e3bcaf838c7a4b52083f3f46b9716dcf639ee4
MagicalSheep <wen_cheng_ke@126.com>
2022-04-09 13:27:47 +0800

    CF1665A

---
+++ pratice/CF1665A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+
+int main()
+{
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        printf("1 %d 1 1\n", n - 3);
+    }
+    return 0;
+}
